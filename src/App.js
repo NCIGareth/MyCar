2fe90379a5ec2523278a48c7cb8babe319476ba4
NCIGareth <x19122748@student.ncirl.ie>
2023-02-28 15:52:42 +0000

    Auth

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
 import Navbar from "./components/Navbar"
 import "./styles/App.scss";
+import SignIn from "./components/Authentication/SignIn";
+import SignUp from "./components/Authentication/SignUp";
+import AuthDetails from "./components/Auth";
 
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
-      <h1>Something goes here</h1>
+      <SignIn/>
+      <SignUp/>
+      <AuthDetails/>
     </div>
 
   );
